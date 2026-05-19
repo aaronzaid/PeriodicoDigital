1cafec095e3c3ec829038d70a119fe8984059ecf
aaronzaid <aaronzaid76@gmail.com>
2026-05-18 22:03:08 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botones = document.querySelectorAll("button");
+
+botones.forEach(boton => {
+  boton.addEventListener("click", () => {
+    alert("Esta sección todavía está en construcción.");
+  });
+});
